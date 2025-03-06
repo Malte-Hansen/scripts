7b1337321c2f97b505c38bfb33c0a3370d85da6a
Malte Hansen <malte.hanseat@gmail.com>
2025-03-06 15:01:03 +0100

    Add script to display PDF metadata

---
+++ pdf/pdf-metadata.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+from PyPDF2 import PdfReader
+
+import argparse
+from datetime import datetime
+
+metadata_keys = ["/Title",
+ "/Author",
+"/Subject",
+"/Keywords",
+"/Creator",
+"/Producer",
+"/CreationDate",
+"/ModDate",
+# "/Trapped"
+]
+
+def read_metadata_in_pdf(file_path):
+    print("\n---", file_path, "---") 
+
+    reader = PdfReader(file_path)
+    meta = reader.metadata
+
+    print(len(reader.pages), "pages")
+    for key in metadata_keys:
+        if key in meta:
+            formatted_value = meta[key] if key.find("Date") == -1 else datetime.strptime(meta[key][2:14], "%Y%m%d%H%M").strftime("%d.%m.%Y %H:%M")
+            print("{}: {}".format(key.replace("/", ""), formatted_value))
+
+def main():
+    parser=argparse.ArgumentParser()
+
+    parser.add_argument("paths", nargs='+', help="List of paths to PDF files, e.g. '*.pdf'")
+
+    args=parser.parse_args()
+
+    for file_path in args.paths:
+        read_metadata_in_pdf(file_path)
+
+if __name__ == "__main__":
+    main()
