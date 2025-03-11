247f8edbed3fe18a20f6c8f881fb90dc610612ff
Malte Hansen <malte.hanseat@gmail.com>
2025-03-11 13:55:11 +0100

    Add script to extract images from PDF

--- pdf/pdf-image-extractor.py
+++ pdf/pdf-image-extractor.py
@@ -10,25 +10,29 @@ def main():
 
     args=parser.parse_args()
 
-    pdf_writer = pypdf.PdfWriter()
-
     for pdf in args.paths:
+        image_folder_path = pdf.replace(".pdf", "") + "-extracted-images"
+        if not os.path.exists(image_folder_path):
+            os.makedirs(image_folder_path)
+        
         pdf_reader = pypdf.PdfReader(pdf)
         print("\n--- {} ---".format(pdf))
-        count = 0
-        for page in range(len(pdf_reader.pages)):
-            print("Images:", page.images)
+        pageNo = 1
+        for page in pdf_reader.pages:
+            count = 0
             for image_file_object in page.images:
-                image_file_path = str(count) + image_file_object.name
+                image_file_name = "page-{}-{}-{}".format(pageNo, count, image_file_object.name)
+                image_file_path = image_folder_path + "/" + image_file_name
                 # Check if output file would overwrite existing file
-                if (os.path.isfile(args.output) and
+                if (os.path.isfile(image_file_path) and
                     input("Image file '{}' already exists. Overwrite? (y/N)".format(image_file_path)) != "y"
                     ):
                     break
                 with open(image_file_path, "wb") as fp:
                     fp.write(image_file_object.data)
                     count += 1
-                    print("Wrote image {} to disk.".format(image_file_path))
+                    print("Page {}: Wrote image {} to disk.".format(pageNo, image_file_name))
+            pageNo += 1
 
 if __name__ == "__main__":
     main()
